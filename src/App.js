957ca897361facc01a8b24c84256700b49ee8c12
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-01 12:22:21 -0800

    Add template files, add profiler and routeswitch.js files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Profiler from "./Profiler"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from App!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Profiler.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function Profiler () {
+    return (
+        <div>
+            <h1>Hello from Profiler</h1>
+        </div>
+    )
+}
