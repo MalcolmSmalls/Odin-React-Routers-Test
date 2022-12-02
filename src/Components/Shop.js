658b20608d3e07f7ebcd4d9dcc5801222b153f8f
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-02 11:02:22 -0800

    Get data from API with async function, use to set state and dynamically produce h1's based on api item names

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,8 +1,25 @@
-import React from "react"
+import React from "react";
 import '../App.css';
 
 export default function Shop(){
+    const [ items, setItems ] = React.useState([])
+
+    React.useEffect (() => {
+        async function apiGrab(){
+            const res = await fetch ('https://fortnite-api.com/v2/shop/br')
+            const data = await res.json()
+            console.log(data.data)
+            setItems(data.data.daily.entries)
+        }
+        apiGrab()
+    }, [])
+
+    // const createTitles = items.map ( item => <h1>{item.items[0].name}</h1>)
     return (
-        <h1>Shop Page</h1>
+        <div>
+            {/* {createTitles} */}
+            {items.map(item => <h1 key = {item.offerId}>{item.items[0].name}</h1>)}
+        </div>
+
     )
 }
